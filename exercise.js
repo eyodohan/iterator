cfcfe0353e6c04f8e0eb7869df6435096b86f18b
eyupcan-dogan <88985629+eyupcan-dogan@users.noreply.github.com>
2021-10-21 19:09:11 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,3 +1,22 @@
+/* function fn (metin) {
+    let diz = []
+    function sirala (){
+        for (let i = 0; i<metin.length; ++i){
+            diz = diz.push(metin[i])
+        }
+        return diz
+    }
+    return sirala
+}
+
+
+const text = "Hello, world"
+const getText = fn(text)
+
+const element1 = getText()
+console.log(element1)
+*/
+
 function fn (metin) {
     let diz = []
     function sirala (){
@@ -15,3 +34,6 @@ const getText = fn(text)
 
 const element1 = getText()
 console.log(element1)
+
+
+
